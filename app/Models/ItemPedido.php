e6fb5f51889b6471e15cac5ba646a74485203a11
DanielGiraldiMichels <danigmichels@gmail.com>
2026-06-17 00:35:29 -0300

    fix: correcao de link do modelo Produto em ItemPedido

--- app/Models/ItemPedido.php
+++ app/Models/ItemPedido.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Pedido;
+use App\Models\Produto;
 
 class ItemPedido extends Model
 {
